09468546c97bbc9fb1b2d01f42b6d719a73a4583
rlarlduf20 <kky8274@naver.com>
2024-10-29 17:04:30 +0900

    Feat: Panda css 초기 세팅

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,9 +1,20 @@
+import { styled } from "../../../styled-system/jsx";
+
 interface Props {
   children: React.ReactNode;
 }
 
 const Button = ({ children }: Props) => {
-  return <button type="button">{children}</button>;
+  return <StyledButton type="button">{children}</StyledButton>;
 };
 
 export default Button;
+
+const StyledButton = styled("button", {
+  base: {
+    width: "150px",
+    height: "80px",
+    backgroundColor: "lightgreen",
+    borderRadius: "10px",
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Button from "./_components/Button";
+
 const MainPage = () => {
-  return <p>Welcome! Wooddk</p>;
+  return (
+    <>
+      <p>Welcome! Wooddk</p>
+      <Button>버튼</Button>
+    </>
+  );
 };
 
 export default MainPage;
