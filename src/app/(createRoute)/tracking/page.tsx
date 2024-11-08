98a15ee923e3e79fd8a2da9cbdb83af45b29577f
rlarlduf20 <kky8274@naver.com>
2024-11-08 16:16:04 +0900

    Feat: 산책끝내기 시 리뷰 스택이 쌓이도록 메시지 전송

--- src/app/(createRoute)/tracking/page.tsx
+++ src/app/(createRoute)/tracking/page.tsx
@@ -1,5 +1,37 @@
+"use client";
+
 const Tracking = () => {
-  return <div style={{ padding: "100px" }}>산책중~~!</div>;
+  const handleClickFinishBtn = () => {
+    if (typeof window !== "undefined" && window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ type: "STACK_REVIEW" }),
+      );
+      return;
+    }
+    alert("산책 끝은 앱에서 사용할 수 있습니다.");
+  };
+  return (
+    <>
+      <div style={{ padding: "100px" }}>산책중~~!</div>
+      <button
+        type="button"
+        onClick={handleClickFinishBtn}
+        style={{
+          margin: "20px",
+          position: "fixed",
+          left: 50,
+          top: 50,
+          zIndex: 1000,
+          backgroundColor: "white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        산책 끝
+      </button>
+    </>
+  );
 };
 
 export default Tracking;
