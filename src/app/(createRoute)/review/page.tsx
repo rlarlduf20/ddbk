35a45c746e90460c0a0fe980c685077f47069409
rlarlduf20 <kky8274@naver.com>
2024-11-12 14:08:33 +0900

    Feat: 후기작성완료 시 메인 탭으로 이동

--- src/app/(createRoute)/review/page.tsx
+++ src/app/(createRoute)/review/page.tsx
@@ -1,5 +1,35 @@
+"use client";
+
 const Review = () => {
-  return <div style={{ padding: "100px" }}>리뷰~!</div>;
+  const handleClickReviewBtn = () => {
+    if (typeof window !== "undefined" && window.ReactNativeWebView) {
+      window.ReactNativeWebView.postMessage(
+        JSON.stringify({ type: "COMPLETE_REVIEW" }),
+      );
+    }
+  };
+  return (
+    <>
+      <div style={{ padding: "100px" }}>리뷰~!</div>
+      <button
+        type="button"
+        onClick={handleClickReviewBtn}
+        style={{
+          margin: "20px",
+          position: "fixed",
+          left: 50,
+          top: 50,
+          zIndex: 1000,
+          backgroundColor: "white",
+          padding: "10px 20px",
+          borderRadius: "5px",
+          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
+        }}
+      >
+        산책 끝
+      </button>
+    </>
+  );
 };
 
 export default Review;
