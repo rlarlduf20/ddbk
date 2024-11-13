83a1c2a1f0499034fed1b65fafdbf7103c5568bd
rlarlduf20 <kky8274@naver.com>
2024-11-13 11:08:04 +0900

    Feat: 마커 초기화

--- src/app/_hooks/useMap.tsx
+++ src/app/_hooks/useMap.tsx
@@ -9,7 +9,7 @@ interface Props {
 const useMap = ({ location }: Props) => {
   const mapRef = useRef<naver.maps.Map | null>(null);
   const polylineRef = useRef<naver.maps.Polyline | null>(null);
-
+  const curPosMarkerRef = useRef<naver.maps.Marker | null>(null);
   const handleScriptLoad = () => {
     const mapOptions = {
       center: new naver.maps.LatLng(location.latitude, location.longitude),
@@ -22,6 +22,11 @@ const useMap = ({ location }: Props) => {
       path: [],
       strokeColor: "#5347AA",
       strokeWeight: 5,
+    });
+
+    curPosMarkerRef.current = new naver.maps.Marker({
+      position: new naver.maps.LatLng(location.latitude, location.longitude),
+      map: mapRef.current,
     });
   };
 
