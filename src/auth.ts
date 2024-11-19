5e3ce91fabbab7c18b4dd260c16b8ad2395a2edd
rlarlduf20 <kky8274@naver.com>
2024-11-19 21:52:39 +0900

    Feat: 스키마 변경, session token 유효기간 설정

--- src/auth.ts
+++ src/auth.ts
@@ -1,18 +1,19 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
 import Kakao from "next-auth/providers/kakao";
 
 import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
+  session: {
+    strategy: "jwt",
+    maxAge: 60 * 60 * 24 * 30,
+  },
   providers: [
     Kakao({
       clientId: process.env.AUTH_KAKAO_ID,
       clientSecret: process.env.AUTH_KAKAO_SECRET,
     }),
-    Credentials({}),
   ],
 });
