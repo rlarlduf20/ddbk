73d62a8b9b0865d28a0fd0bde6060b3470dbdffe
rlarlduf20 <kky8274@naver.com>
2024-11-20 17:33:37 +0900

    Feat: 로그인 여부 문구 추가

--- src/app/(my)/mypage/page.tsx
+++ src/app/(my)/mypage/page.tsx
@@ -1,55 +1,13 @@
-"use client";
+import GPSGroup from "../_components/GPSGroup";
 
-import { signOut, useSession } from "next-auth/react";
-
-import { logOut } from "@/app/_actions/auth";
 import Typography from "@/app/_components/Typography";
-import usePermissionState from "@/app/_hooks/usePermissionState";
+import { auth } from "@/auth";
 
-const MyPage = () => {
-  const session = useSession();
-
-  const { isPossibleLocationService, isPossiblePermissions } =
-    usePermissionState();
-
-  const handlePermission = () => {
-    if (typeof window !== "undefined" && window.ReactNativeWebView) {
-      window.ReactNativeWebView.postMessage(
-        JSON.stringify({ type: "REQUEST_GPS_PERMISSIONS" }),
-      );
-    }
-  };
-
-  const handleLogOut = async () => {
-    if (typeof window !== "undefined" && window.ReactNativeWebView) {
-      await signOut();
-      window.ReactNativeWebView.postMessage(
-        JSON.stringify({ type: "LOG_OUT" }),
-      );
-      return;
-    }
-    logOut();
-  };
+const MyPage = async () => {
+  const session = await auth();
   return (
     <div style={{ paddingTop: "100px" }}>
-      <button
-        type="button"
-        style={{ border: "1pxSolidBlack", padding: "5px" }}
-        onClick={handlePermission}
-      >
-        {`디바이스 위치서비스 : `}
-        {isPossibleLocationService ? "켜짐" : "꺼짐"}
-      </button>
-      <button
-        type="button"
-        style={{ borderBottom: "1pxSolidBlack", padding: "5px" }}
-        onClick={handlePermission}
-      >
-        {`앱 위치 권한 허용 : `}
-        {isPossiblePermissions ? "켜짐" : "꺼짐"}
-      </button>
-
-      <Typography.H1 color="main_pink">오뚜~~ HI</Typography.H1>
+      {session && <Typography.H1 color="main_pink">로그인중~!</Typography.H1>}
       <Typography.H2 color="main_green">오뚜~~ HI</Typography.H2>
       <Typography.H3>오뚜~~ HI</Typography.H3>
       <Typography.H4>오뚜~~ HI</Typography.H4>
@@ -57,11 +15,7 @@ const MyPage = () => {
       <Typography.PLarge>오뚜~~ HI</Typography.PLarge>
       <Typography.PMedium>오뚜~~ HI</Typography.PMedium>
       <Typography.PSmall>오뚜~~ HI</Typography.PSmall>
-      {session.data?.user?.name}
-
-      <button type="button" onClick={handleLogOut}>
-        버튼
-      </button>
+      <GPSGroup />
     </div>
   );
 };
