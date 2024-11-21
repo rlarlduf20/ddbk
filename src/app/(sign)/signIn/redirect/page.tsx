ace48c8f8138607d86957d33a93829562d69189d
rlarlduf20 <kky8274@naver.com>
2024-11-21 13:53:40 +0900

    Fix: 로그인 여부 앱으로 전송 방식 변경 - signIn함수 호출 후 message 전송 -> /signIn/redirect페이지로 redirect, 앱에서는 웹뷰의 url 변동사항 파악

--- src/app/(sign)/signIn/redirect/page.tsx
+++ src/app/(sign)/signIn/redirect/page.tsx
@@ -1,28 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-
-import { auth } from "@/auth";
-
 const SignInRedirectPage = () => {
-  useEffect(() => {
-    const checkLogin = async () => {
-      const session = await auth();
-      window.ReactNativeWebView.postMessage(
-        JSON.stringify({
-          type: "CHECK_SESSION",
-          user_id: session?.user && session?.user.id,
-        }),
-      );
-      if (session) {
-        window.ReactNativeWebView.postMessage(
-          JSON.stringify({ type: "LOGIN_SUCCESS" }),
-        );
-      }
-    };
-    checkLogin();
-  }, []);
-
   return <p style={{ padding: "100px" }}>로그인중입니다.....</p>;
 };
 
