c66643d4331fb1f59a1cdb8f5f57f47c5c291417
rlarlduf20 <kky8274@naver.com>
2024-12-03 16:58:36 +0900

    Feat: 클라이언트사이드 데이터패칭- 리액트 쿼리 도입

--- src/app/(my)/mypage/page.tsx
+++ src/app/(my)/mypage/page.tsx
@@ -1,11 +1,26 @@
-import Typography from "@/app/_components/Typography";
-import { auth } from "@/auth";
+"use client";
 
-const MyPage = async () => {
-  const session = await auth();
+import { useQuery } from "@tanstack/react-query";
+
+import Loading from "@/app/_components/Loading";
+import Typography from "@/app/_components/Typography";
+
+const MyPage = () => {
+  const { data: user, isLoading } = useQuery({
+    queryKey: ["user-nickname"],
+    queryFn: async () => {
+      const res = await fetch("/api/user");
+      const data = await res.json();
+
+      return data;
+    },
+  });
+
+  if (isLoading) return <Loading />;
+
   return (
     <div style={{ paddingTop: "100px" }}>
-      {session && <Typography.H1>로그인중~!</Typography.H1>}
+      <Typography.H1 color="oddu_green01">{user.user.name}</Typography.H1>
       <Typography.H2>오뚜~~ HI</Typography.H2>
       <Typography.H3>오뚜~~ HI</Typography.H3>
       <Typography.H4>오뚜~~ HI</Typography.H4>
