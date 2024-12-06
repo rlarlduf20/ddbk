e646d6901cb7686815d96a84f75e8c21c93dbb71
rlarlduf20 <kky8274@naver.com>
2024-12-06 17:12:48 +0900

    Feat: 로그인 페이지 퍼블리싱

--- src/app/(sign)/signIn/page.tsx
+++ src/app/(sign)/signIn/page.tsx
@@ -1,14 +1,40 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+
 import { css } from "../../../../styled-system/css";
+import { vstack } from "../../../../styled-system/patterns";
 
 import { logIn } from "@/app/_actions/auth";
+import Logo from "@/assets/images/logo-1st.png";
 
-const signContainerStyles = css({
+const signContainerStyles = vstack({
   width: "100%",
-  display: "flex",
-  mt: "100px",
+  mt: "256px",
+  gap: "157px",
   justifyContent: "center",
+});
+
+const signBoxStyles = vstack({
+  gap: "17px",
+});
+
+const credentialButtonStyles = css({
+  width: "335px",
+  height: "60px",
+  bgColor: "oddu_green01",
+  borderRadius: "12px",
+  color: "oddu_white01",
+  cursor: "pointer",
+});
+
+const kakaoButtonStyles = css({
+  width: "335px",
+  height: "60px",
+  bgColor: "#FEE500",
+  borderRadius: "12px",
+  cursor: "pointer",
 });
 
 const SignIn = () => {
@@ -18,9 +44,21 @@ const SignIn = () => {
 
   return (
     <div className={signContainerStyles}>
-      <button type="button" onClick={handleLogin}>
-        카카오 로그인
-      </button>
+      <Link href="/">
+        <Image src={Logo} alt="로고" width={200} height={100} />
+      </Link>
+      <div className={signBoxStyles}>
+        <button type="button" className={credentialButtonStyles}>
+          아이디로 로그인하기
+        </button>
+        <button
+          type="button"
+          className={kakaoButtonStyles}
+          onClick={handleLogin}
+        >
+          카카오 로그인
+        </button>
+      </div>
     </div>
   );
 };
