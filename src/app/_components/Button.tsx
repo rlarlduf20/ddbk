eaae30e0f43a08b22b0476f267531e2c003e06bb
rlarlduf20 <kky8274@naver.com>
2024-12-06 18:38:32 +0900

    Feat: 공통 버튼 컴포넌트 수정

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,63 +1,53 @@
+import Typography from "./Typography";
 import { cva } from "../../../styled-system/css";
 
-// 버튼 스타일 정의
 const buttonStyles = cva({
   base: {
     display: "inline-flex",
     alignItems: "center",
     justifyContent: "center",
     cursor: "pointer",
-    transition: "all 0.3s ease",
     border: "none",
-    padding: "8px 16px",
   },
   variants: {
-    visual: {
-      outline: {
-        bg: "white",
-        border: "1px solid black",
-      },
-    },
     color: {
-      main_pink: { color: "main_pink" },
-      main_green: { color: "main_green" },
-      black: { color: "black" },
-      white: { color: "white" },
+      oddu_black03: { bgColor: "oddu_black03" },
+      oddu_green01: { color: "oddu_green01" },
     },
     size: {
-      small: { fontSize: "12px", padding: "4px 8px" },
-      medium: { fontSize: "16px", padding: "8px 16px" },
-      large: { fontSize: "20px", padding: "12px 24px" },
+      large: { width: "335px", height: "60px" },
+      medium: { width: "164px", height: "60px" },
+      small: { width: "100px", height: "52px" },
     },
     rounded: {
       true: { borderRadius: "50%" },
-      false: { borderRadius: "4px" },
+      false: { borderRadius: "30px" },
     },
   },
 });
 
 interface ButtonProps {
-  color?: "main_pink" | "main_green" | "black" | "white";
+  color?: "oddu_green01" | "oddu_black03";
   size?: "small" | "medium" | "large";
   rounded?: boolean;
-  visual?: "outline";
   children: React.ReactNode;
+  handleClick: any;
 }
 
-// 동적으로 스타일을 관리하는 버튼 컴포넌트
 const Button = ({
-  color = "black",
+  color = "oddu_black03",
   size = "medium",
   rounded = false,
   children,
-  visual,
+  handleClick,
 }: ButtonProps) => {
   return (
     <button
       type="button"
-      className={buttonStyles({ color, size, rounded, visual })}
+      className={buttonStyles({ color, size, rounded })}
+      onClick={handleClick}
     >
-      {children}
+      <Typography.PMedium color="oddu_white01">{children}</Typography.PMedium>
     </button>
   );
 };
