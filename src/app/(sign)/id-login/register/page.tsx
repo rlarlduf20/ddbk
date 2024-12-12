771275f9c388a22ed79e1710837cedf59924b3a4
rlarlduf20 <kky8274@naver.com>
2024-12-12 15:29:44 +0900

    Feat: 아이디로그인 페이지 최소높이 지정

--- src/app/(sign)/id-login/register/page.tsx
+++ src/app/(sign)/id-login/register/page.tsx
@@ -1,10 +1,15 @@
+import { css } from "../../../../../styled-system/css";
 import RegisterForm from "../../_components/RegisterForm";
 
 import AppBar from "@/app/_components/AppBar";
 
+const registerContainerStyles = css({
+  minH: "812px",
+});
+
 const IDRegisterPage = () => {
   return (
-    <div>
+    <div className={registerContainerStyles}>
       <AppBar>회원가입</AppBar>
       <RegisterForm />
     </div>
