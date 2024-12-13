a18dced60ff00af4b21230f1b168dddd9041a887
rlarlduf20 <kky8274@naver.com>
2024-12-13 16:28:58 +0900

    Feat: 데이터 불러오는 과정에서 에러 발생 시 에러페이지 리다이렉트

---
+++ src/app/(main)/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>{error.message}</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        새로고침
+      </button>
+    </div>
+  );
+}
--- src/app/_actions/user.ts
+++ src/app/_actions/user.ts
@@ -1,12 +1,12 @@
 import { prisma } from "@/prisma";
 
 export const getUser = async (userId: string | undefined) => {
-  if (!userId) return null;
-
   const user = await prisma.user.findUnique({
     where: { id: userId },
     select: { name: true },
   });
+
+  if (!user) throw new Error("유효하지 않은 접근입니다.");
 
   return user;
 };
