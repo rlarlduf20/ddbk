fc9020e922d54954ac08875c195d6133e8ec8471
rlarlduf20 <kky8274@naver.com>
2024-12-17 22:13:18 +0900

    Feat: 발도장 마커 임시 추가

--- src/app/_hooks/useMap.tsx
+++ src/app/_hooks/useMap.tsx
@@ -6,10 +6,23 @@ interface Props {
   location: { latitude: number; longitude: number };
 }
 
+const footprints = [
+  {
+    title: "웩슨 출몰 지역",
+    position: { latitude: 37.5, longitude: 126.97 },
+  },
+  {
+    title: "경복궁 카페",
+    position: { latitude: 37.579293849225, longitude: 126.97798076343 },
+  },
+];
+
 const useMap = ({ location }: Props) => {
   const mapRef = useRef<naver.maps.Map | null>(null);
   const polylineRef = useRef<naver.maps.Polyline | null>(null);
   const curPosMarkerRef = useRef<naver.maps.Marker | null>(null);
+
+  const footprintMarkerRef = useRef<naver.maps.Marker | null>(null);
 
   const handleScriptLoad = () => {
     const mapOptions = {
@@ -23,6 +36,26 @@ const useMap = ({ location }: Props) => {
       path: [],
       strokeColor: "#5347AA",
       strokeWeight: 5,
+    });
+
+    footprints.forEach((footprint: any) => {
+      if (!mapRef.current) return;
+
+      footprintMarkerRef.current = new naver.maps.Marker({
+        position: new naver.maps.LatLng(
+          footprint.position.latitude,
+          footprint.position.longitude,
+        ),
+        map: mapRef.current,
+        title: footprint.name,
+        icon: {
+          url: "/marker-footprint.svg",
+          size: new naver.maps.Size(50, 63),
+          scaledSize: new naver.maps.Size(50, 63),
+          origin: new naver.maps.Point(0, 0),
+          anchor: new naver.maps.Point(12, 37),
+        },
+      });
     });
 
     curPosMarkerRef.current = new naver.maps.Marker({
