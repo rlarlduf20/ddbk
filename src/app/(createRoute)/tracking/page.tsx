4a6d900335e5d04fde276af2f601585ee767327b
rlarlduf20 <kky8274@naver.com>
2024-12-19 20:02:13 +0900

    Feat: tracking페이지 단계 state 생성

--- src/app/(createRoute)/tracking/page.tsx
+++ src/app/(createRoute)/tracking/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+
+import { useState } from "react";
+
 import MapContainer from "../_components/MapContainer";
 
+type TrackingPhaseTypes = "notice" | "timer" | "tracking";
+
 const Tracking = () => {
+  const [trackingPhase, setTrackingPhase] =
+    useState<TrackingPhaseTypes>("notice");
+
+  const changeTrackingPhase = (step: TrackingStepTypes) => {
+    setTrackingPhase(step);
+  };
   return <MapContainer />;
 };
 
