16ae4e81bbc96da87cb6b5e4fd3f7a5aa28dc972
rlarlduf20 <kky8274@naver.com>
2024-12-27 18:07:35 +0900

    Feat: 리뷰페이지 틀 구성

--- src/app/(createRoute)/review/page.tsx
+++ src/app/(createRoute)/review/page.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import { css } from "../../../../styled-system/css";
+
+import AppBar from "@/app/_components/AppBar";
+import Button from "@/app/_components/Button";
+
+const buttonBoxStyles = css({
+  position: "absolute",
+  bottom: "30px",
+  left: "50%",
+  transform: "translate(-50%, 0)",
+});
+
 const Review = () => {
   const handleClickReviewBtn = () => {
     if (typeof window !== "undefined" && window.ReactNativeWebView) {
@@ -10,24 +22,16 @@ const Review = () => {
   };
   return (
     <>
-      <div style={{ padding: "100px" }}>리뷰~!</div>
-      <button
-        type="button"
-        onClick={handleClickReviewBtn}
-        style={{
-          margin: "20px",
-          position: "fixed",
-          left: 50,
-          top: 50,
-          zIndex: 1000,
-          backgroundColor: "white",
-          padding: "10px 20px",
-          borderRadius: "5px",
-          boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)",
-        }}
-      >
-        산책 끝
-      </button>
+      <AppBar>산책 종료</AppBar>
+      <div className={buttonBoxStyles}>
+        <Button
+          size="large"
+          color="oddu_green01"
+          handleClick={handleClickReviewBtn}
+        >
+          종료
+        </Button>
+      </div>
     </>
   );
 };
